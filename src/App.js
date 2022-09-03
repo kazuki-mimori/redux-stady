3a6342524611f243ef7aca29ed0a4bac48f43f06
mimori <kazumasa071210@gmail.com>
2022-09-04 08:10:46 +0900

    ⬆️ jsonでデータを取得

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import Post from "./components/post";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Redux Learn</h1>
+      <Post />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/store/index.js
@@ -0,0 +1,19 @@
+import { createStore } from "redux";
+
+const initialState = {
+  posts: [],
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "GET_POST_DATA":
+      return { ...state, posts: action.payload };
+
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
+export default store;
